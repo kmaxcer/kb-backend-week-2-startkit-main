744cc1b88cbb2cf58100311249bbae7c3c544a1d
kmaxcer <149956842+kmaxcer@users.noreply.github.com>
2024-04-14 11:33:59 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const staticFile = require('./appModules/http-utils/static-file')
+const path = require('path')
+const mimeTypes = require('./appModules/http-utils/mime-types')
+const mainRouteController = require('./controllers/main')
+const defaultRouteController = require('./controllers/default')
+const gameRouteController = require('./controllers/game')
+const voteRouteController = require('./controllers/vote')
+const server = http.createServer((req, res) => {
+    const url = req.url
+
+    switch (url) {
+        case "/":
+            mainRouteController(res, "/index.html", ".html");
+            break;
+        case "/game":
+            gameRouteController(res);
+            break;
+        case "/vote":
+            voteRouteController(req, res);
+            break;         
+        default:
+            defaultRouteController(res, url);
+    }
+});
+
+server.listen(3005, () => {
+    console.log('Server is running on port 3005');
+});
---
+++ appModules/api/config.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://api-code-2.practicum-team.ru'
+const endpoints = {
+    games: `${BASE_URL}/games`
+}
+
+module.exports = endpoints;
+
+const WEIGHT = {
+    gameplay: 2,
+    design: 1,
+    idea: 3,
+  }; 
